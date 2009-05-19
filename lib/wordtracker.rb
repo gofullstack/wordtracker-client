a3b584ac7eea96a8e638caa8d88db86c7c00ed7d
nsmith <nsmith@be0ae9cb-b933-8549-b8e1-97761fd10f02>
2009-05-19 19:45:02 +0000

    Added initial functionality
    git-svn-id: http://projects.cramerdev.net/svn/wordtracker/trunk@5038 be0ae9cb-b933-8549-b8e1-97761fd10f02

--- lib/wordtracker.rb
+++ lib/wordtracker.rb
@@ -1,6 +1,55 @@
+require 'xmlrpc/client'
+require 'rubygems'
+require 'active_support'
+
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module Wordtracker
+  include ActiveSupport
   VERSION = '0.0.1'
+  
+  # Error class
+  class FaultException < XMLRPC::FaultException; end
+
+  class Client < XMLRPC::Client
+
+    def initialize(id = "guest", options = {})
+      options.symbolize_keys!
+      # Use the dummy API if no ID is provided
+      @id = id
+      url = @id == "guest" ? "test.xmlrpc.wordtracker.com" :
+        "xmlrpc.wordtracker.com"
+      super(url, "/")
+    end
+
+    # See if the service responds
+    def ping
+      send("ping")
+    end
+    
+    # Get a list of related keyphrases and their occurences when searching
+    def get_lateral_keyphrases(options = {})
+      options.symbolize_keys!
+      phrases = {}
+      send("get_lateral_keyphrases", 
+               options[:keyphrases] || [],
+               !!options[:include_plurals],
+               options[:adult_filter] || "off",
+               options[:max] || 0,
+               options[:timeout] || 0)
+    end
+    
+    private 
+      # Send a call to the service using the id
+      def send(*args)
+        args = args.insert(1, @id) # Add the id in the arguments
+        meth = args[0] # Method is the 1st argument
+        begin
+          self.call(*args)
+        rescue XMLRPC::FaultException => e
+          raise Wordtracker::FaultException.new(0, "RPC error calling #{meth}")
+        end
+      end
+  end
 end
--- test/test_wordtracker.rb
+++ test/test_wordtracker.rb
@@ -3,9 +3,33 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 class TestWordtracker < Test::Unit::TestCase
 
   def setup
+    # Create a guest instance on each test
+    @w = Wordtracker::Client.new
   end
   
-  def test_truth
-    assert true
+  def teardown
+  end
+
+  def test_initialize
+    # Creating a new instance with no arguments should work
+    assert @w.class == Wordtracker::Client
+  end
+
+  def test_ping
+    # Call to ping service should return true
+    assert @w.ping
+  end
+
+  def test_bad_ping
+    # Request with invalid key should raise an error
+    bad_client = Wordtracker::Client.new("bad")
+    assert_raise Wordtracker::FaultException do
+      bad_client.ping
+    end
+  end
+
+  def test_get_lateral_keyphrases
+    assert_equal @w.get_lateral_keyphrases(:keyphrases => ["cats", "kittens"]),
+      { "cats" => 999, "kittens" => 999 }
   end
 end
